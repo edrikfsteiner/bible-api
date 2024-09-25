afde110913bb372c532bcd97cdd9a5243a8d3fc5
edrikfsteiner <edrikfsteiner@gmail.com>
2024-09-25 17:33:28 -0300

    Initial Commit

---
+++ src/test/java/com/bible/bibleAPI/BibleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bible.bibleAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
