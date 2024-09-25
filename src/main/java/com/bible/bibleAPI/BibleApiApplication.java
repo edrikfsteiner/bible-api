e9d4a07e9bd63f57cd4a275e04a4ca09c67414f7
edrikfsteiner <edrikfsteiner@gmail.com>
2024-09-25 17:35:09 -0300

    Update Application

---
+++ src/main/java/com/bible/bibleAPI/BibleApiApplication.java
@@ -0,0 +1,11 @@
+package com.bible.bibleAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BibleApiApplication.class, args);
+	}
+}
